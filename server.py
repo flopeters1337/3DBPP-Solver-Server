ce964e4e6268b18628580de3f177a92eb1317d24
Florian Peters <fl0wryan@hotmail.com>
2020-11-09 09:45:53 +0100

    Functional server + Add request and response examples

--- server.py
+++ server.py
@@ -1,7 +1,22 @@
 
-from solvers import DummySolver
-from flask import Flask, request
+import flask.json
+import decimal
+from solvers import SimpleSolver, GenericBin, GenericItem
+from flask import Flask, request, jsonify
+
+
+class DecimalJSONEncoder(flask.json.JSONEncoder):
+    """
+    Custom JSON encoder class to handle decimal numbers
+    """
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        return super(DecimalJSONEncoder, self).default(o)
+
+
 app = Flask(__name__)
+app.json_encoder = DecimalJSONEncoder
 
 
 @app.route('/')
@@ -11,14 +26,24 @@ def hello_world():
 
 @app.route('/solve', methods=['POST'])
 def solve_instance():
-    crates_list = parse_json(request.form['crates'])
-    return solver.solve(crates_list)
+    bins, items = parse_json(request.json)
+    solution = solver.solve(bins, items)
+    return jsonify(solution)
 
 
 def parse_json(json_data):
-    return json_data    # TODO: document JSON data type
+    bins = list()
+    items = list()
+
+    for bin in json_data['bins']:
+        bins.append(GenericBin(bin['name'], bin['width'], bin['height'], bin['depth'], bin['max_weight']))
+
+    for item in json_data['items']:
+        items.append(GenericItem(item['name'], item['width'], item['height'], item['depth'], item['weight']))
+
+    return bins, items
 
 
 if __name__ == '__main__':
-    solver = DummySolver()
+    solver = SimpleSolver()
     app.run(host='0.0.0.0')
--- solvers.py
+++ solvers.py
@@ -1,5 +1,6 @@
 
 from abc import ABC, abstractmethod
+from py3dbp import Bin, Item, Packer
 
 
 class AbstractSolver(ABC):
@@ -7,7 +8,7 @@ class AbstractSolver(ABC):
         pass
 
     @abstractmethod
-    def solve(self, crates_list):
+    def solve(self, bins, items):
         return None
 
 
@@ -15,13 +16,86 @@ class DummySolver(AbstractSolver):
     def __init__(self):
         pass    # TODO
 
-    def solve(self, crates_list):
+    def solve(self, bins, items):
         pass    # TODO
 
 
 class SimpleSolver(AbstractSolver):
     def __init__(self):
-        pass    # TODO
+        super().__init__()
+        self.packer = Packer()
 
-    def solve(self, crates_list):
-        pass    # TODO
+    @staticmethod
+    def convert_rotation(rotation_type):
+        rotation = (0, 0, 0)
+
+        if rotation_type == 1:
+            rotation = (90, 0, 0)
+        elif rotation_type == 2:
+            rotation = (0, 90, 0)
+        elif rotation_type == 3:
+            rotation = (0, 90, 90)
+        elif rotation_type == 4:
+            rotation = (0, 0, 90)
+        elif rotation_type == 5:
+            rotation = (90, 0, 90)
+
+        return rotation
+
+    def solve(self, bins, items):
+        for bin in bins:
+            self.packer.add_bin(Bin(bin.name, bin.width, bin.height, bin.depth, bin.max_weight))
+
+        for item in items:
+            self.packer.add_item(Item(item.name, item.width, item.height, item.depth, item.weight))
+
+        self.packer.pack(distribute_items=True)
+
+        return self.parse_solution()
+
+    def parse_solution(self):
+        solution = {}
+
+        bins = []
+        for bin in self.packer.bins:
+            items = []
+            for item in bin.items:
+                rotation_coords = self.convert_rotation(item.rotation_type)
+                items.append(
+                    {
+                        "name": item.name,
+                        "width": item.width,
+                        "height": item.height,
+                        "depth": item.depth,
+                        "x": item.position[2],  # Because Z and X axes swapped for Unity
+                        "y": item.position[1],
+                        "z": item.position[0],
+                        "rotation_x": rotation_coords[0],
+                        "rotation_y": rotation_coords[1],
+                        "rotation_z": rotation_coords[2]
+                    }
+                )
+
+            bins.append({"name": bin.name, "items": items})
+
+        solution['bins'] = bins
+
+        return solution
+
+
+class GenericBin:
+    def __init__(self, name, width, height, depth, max_weight):
+        self.name = name
+        self.width = width
+        self.height = height
+        self.depth = depth
+        self.max_weight = max_weight
+
+
+class GenericItem:
+    def __init__(self, name, width, height, depth, weight):
+        self.name = name
+        self.width = width
+        self.height = height
+        self.depth = depth
+        self.weight = weight
